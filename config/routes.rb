01a0b045ef517acefb113bf34f99d0348a12e513
Your Name <21012040@st.phenikaa-uni.edu.vn>
2025-03-04 15:02:56 +0700

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit--no-test-framework'
   # root "articles#index"
   root "static_pages#home"
 
@@ -9,7 +11,8 @@ Rails.application.routes.draw do
   get    '/signup',  to: 'users#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
-  get '/logout',     to: 'sessions#destroy'
+  get    '/logout',  to: 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_03_04_044605) do
+ActiveRecord::Schema[7.0].define(version: 2025_03_04_075145) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2025_03_04_044605) do
     t.string "activation_digest"
     t.boolean "activated", default: false
     t.datetime "activated_at", precision: nil
+    t.string "reset_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
